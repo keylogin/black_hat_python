a9d3c53f8b4d90d7a044b12c36d4cc9562dd65a7
keylogin <seriousam111@gmail.com>
2025-01-28 17:13:21 +0200

    ssh_rcmd and ssh_server

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -9,12 +9,15 @@ HEX_FILTER = ''.join(
 def hexdump(src, length=16, show=True):
     if isinstance(src, bytes):
         src = src.decode()
+        
     results = list()
     for i in range(0, len(src), length):
         word = str(src[i:i+length])
+        
         printable = word.translate(HEX_FILTER)
         hexa = ' '.join([f'{ord(c):02X}' for c in word])
         hexwidth = length*3
+        
         results.append(f'{i:04x}  {hexa:<{hexwidth}}  {printable}')
     if show:
         for line in results:
